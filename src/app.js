c2a36e2d203fa271f8b0dc3029de01e879da7489
Jaz Bedoya <jbedoya628@gmail.com>
2025-06-12 13:44:41 +0200

    Seconds commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById('excuse').innerHTML = generateExcuse;
 };
+
+//Excusas en diferentes listas (arrays)
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+//Funcion que elige un numero aleatorio
+function randomIndex(array){
+  return Math.floor(Math.random() * array.length);
+}
+
+
+
+
+
+
+
+// funcion que genera la excusa
+function generateExcuse() {
+  let excuse =
+    who[randomIndex(who)] + '  '+
+    action[randomIndex(action)]+ '  '+
+    what[randomIndex(what)]+'  '+
+    when[randomIndex(when)];
+  return excuse
+
+
+}
